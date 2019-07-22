886fa958f0ca3bd2eaf96fbfb67d647cce80e857
Takahiro Uchiyama <takahiro.uchiyama@livesense.co.jp>
2019-07-22 19:43:41 +0900

    Add my meishi setting

--- keyboards/meishi/keymaps/default/keymap.c
+++ keyboards/meishi/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT( /* Base */
-  LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V) \
+   LCTL(LGUI(KC_Q)), KC_MRWD, KC_MPLY, KC_MFFD \
 ),
 };
 
