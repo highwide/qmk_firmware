59807f23c1837d035ab7ebbf7fe84f9fb6ff1acf
highwide <hochweit728@gmail.com>
2019-07-28 01:28:37 +0900

    Tidy up a layout

--- keyboards/mint60/keymaps/highwide/keymap.c
+++ keyboards/mint60/keymaps/highwide/keymap.c
@@ -33,25 +33,25 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,  \
-    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
-    MO(1),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
-    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(2),   \
-    KC_ESC,    KC_LCTL,    KC_LALT,    KC_LANG2,    KC_LGUI,        KC_SPC,              KC_LANG1,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT \`
+    KC_GRV,   KC_1,      KC_2,      KC_3,      KC_4,      KC_5,           KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,  KC_EQL,    KC_BSPC,  \
+    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,           KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,  KC_RBRC,   KC_BSLS, \
+    MO(1),      KC_A,      KC_S,      KC_D,      KC_F,      KC_G,           KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,  K\
+    KC_LSFT,     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,           KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,  KC_RSFT,  KC_UP,    MO(2),   \
+    KC_ESC,  KC_LCTL,       KC_LALT,    KC_LANG2,            KC_LGUI,        KC_SPC,                KC_LANG1,  KC_RGUI,            KC_LEFT, KC_DOWN,   KC_RGHT  \
   ),
   [1] = LAYOUT( \
-    XXXXXXX,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    XXXXXXX, \
-    XXXXXXX,   XXXXXXX,  LCTL(KC_W), KC_END, XXXXXXX, LCTL(KC_T),   XXXXXXX, LCTL(KC_U), KC_TAB, XXXXXXX, KC_UP, KC_ESC, XXXXXXX, XXXXXXX, \
-    XXXXXXX,     KC_HOME, XXXXXXX, LCTL(KC_D), KC_RGHT, XXXXXXX,   KC_BSPC, XXXXXXX, LCTL(KC_K), LCTL(KC_L), XXXXXXX, XXXXXXX,        XXXXXXX, \
-    _______,       XXXXXXX, XXXXXXX, LCTL(KC_C), XXXXXXX, KC_LEFT,   KC_DOWN, KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, _______, \
-    XXXXXXX,   _______,    _______,    _______,    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_PGDN, XXXXXXX \
-  )
+    XXXXXXX,  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,          KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,   KC_F12,    XXXXXXX,  \
+    XXXXXXX,   XXXXXXX,   LCTL(KC_W),KC_END,    XXXXXXX,   LCTL(KC_T),     XXXXXXX,   LCTL(KC_U),KC_TAB,    XXXXXXX,   KC_UP,     KC_ESC,   XXXXXXX,   XXXXXXX, \
+    XXXXXXX,    KC_HOME,   XXXXXXX,   LCTL(KC_D),KC_RGHT,   XXXXXXX,        KC_BSPC,   XXXXXXX,   LCTL(KC_K),LCTL(KC_L),XXXXXXX,   XXXXXXX,  XXXXXXX,           \
+    _______,     XXXXXXX,   XXXXXXX,   LCTL(KC_C),XXXXXXX,   KC_LEFT,        KC_DOWN,   KC_ENT,    XXXXXXX,   XXXXXXX,   XXXXXXX,  _______,  KC_PGUP,  _______, \
+    XXXXXXX, _______,       _______,    _______,             XXXXXXX,        XXXXXXX,               XXXXXXX,   XXXXXXX,            XXXXXXX,  KC_PGDN,  XXXXXXX  \
+  ),
   [2] = LAYOUT( \
-    XXXXXXX,  RGB_TOG,   RGBRST,   RGB_HUI,   RGB_SAI,   RGB_VAI,     RGB_MOD,   RGB_HUD,   RGB_SAD,   RGB_VAD,   XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX, \
-    XXXXXXX,   KC_MRWD,  KC_MPLY, KC_MFFD, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX,     KC__VOLDOWN, KC__VOLUP, KC__MUTE, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, LCTL(LGUI(KC_Q)), XXXXXXX, XXXXXXX,        XXXXXXX, \
-    _______,       KC_BRIU, KC_BRID, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, _______, \
-    XXXXXXX,   _______,    _______,    _______,    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_END \
+    XXXXXXX,  RGB_TOG,   RGBRST,   RGB_HUI,   RGB_SAI,   RGB_VAI,         RGB_MOD,   RGB_HUD,   RGB_SAD,   RGB_VAD,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  \
+    XXXXXXX,   KC_MRWD,   KC_MPLY,  KC_MFFD,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX, \
+    XXXXXXX,    KC__VOLDOWN,KC__VOLUP,KC__MUTE, XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,LCTL(LGUI(KC_Q)),XXXXXXX,XXXXXXX,   XXXXXXX,          \
+    _______,     KC_BRIU,    KC_BRID,  XXXXXXX,  XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,   _______,   KC_PGUP, _______, \
+    XXXXXXX, _______,        _______,   _______,            XXXXXXX,         XXXXXXX,               XXXXXXX,   XXXXXXX,            KC_HOME,   KC_PGDN, KC_END   \
   )
 };
 
